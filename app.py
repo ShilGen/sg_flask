c6c6fba84e572190b0c6228abe928a9bb0a0136f
Shilov <geniusshil@gmail.com>
2024-12-01 02:25:46 +0300

    add SCRF

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
-from flask import session, request, redirect, url_for
+from flask import session, request, redirect, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 @app.route("/")
 def index():
@@ -13,19 +19,13 @@ def index():
         return f'Logged in as {session["username"]}  <br><a href="/logout">go to</a>'
     return 'You are not logged <br><a href="/login">go to</a>'
 
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    form = LoginForm()
+    if form.validate_on_submit():
         session["username"] = request.form["username"]
         return redirect(url_for("index"))
-    return """
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    """
-
+    return render_template('login.html', form=form)
 
 @app.route("/logout")
 def logout():
