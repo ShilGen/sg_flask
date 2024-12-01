42e975d5f22581d4ae1de6ca9f6b37e6de8ad218
Shilov <geniusshil@gmail.com>
2024-12-01 03:04:40 +0300

    add csrf

--- app.py
+++ app.py
@@ -3,15 +3,23 @@ from flask import session, request, redirect, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect, CSRFError
 
 
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['TESTING'] = True
+csrf = CSRFProtect(app)
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    username = StringField("Username", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+@app.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    return render_template('csrf_error.html', reason=e.description), 400
+
 
 @app.route("/")
 def index():
@@ -19,13 +27,15 @@ def index():
         return f'Logged in as {session["username"]}  <br><a href="/logout">go to</a>'
     return 'You are not logged <br><a href="/login">go to</a>'
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         session["username"] = request.form["username"]
         return redirect(url_for("index"))
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
+
 
 @app.route("/logout")
 def logout():
